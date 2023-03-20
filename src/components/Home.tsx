9c33445a09d9cfea102a742577195d8cb5166b6a
Alvaro Falcon Morales <alvaro.falconm93@gmail.com>
2023-03-20 23:49:06 +0000

    adding navbar and home color

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,9 @@ import { PropsWithChildren } from "react";
 import Navbar from "./Navbar";
 
 function Container(props: PropsWithChildren) {
-  return <div className={"h-screen"}>{props.children}</div>;
+  return (
+    <div className={"h-screen bg-primary text-white"}>{props.children}</div>
+  );
 }
 export default function Home() {
   return (
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <nav className={"flex flex-row justify-between p-2"}>
+    <nav className={"flex flex-row justify-between p-2 bg-black"}>
       <span>{"{ Alvaro }"}</span>
       <ul className={"flex flex-row gap-4"}>
         <li>About</li>
