0a369e8bdf9ebcb13cf37c6411ddac342047ac19
Alvaro Falcon Morales <alvaro.falconm93@gmail.com>
2023-04-24 21:48:56 +0100

    feat: using data source with projects section

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -1,12 +1,16 @@
 import TechnologiesContainer from "./TechnologiesContainer";
-import data from "../data-source/projects.json";
-export default function ProjectItem() {
+type Props = {
+  image: string;
+  technologies: string[];
+  title: string;
+};
+export default function ProjectItem({ image, technologies, title }: Props) {
   return (
     <div className={"flex flex-col text-center gap-2"}>
-      <img src={data.image} alt="" className={"rounded-[5%]"} />
-      <h2 className={"font-bold text-dark"}>{data.title}</h2>
+      <img src={image} alt="" className={"rounded-[5%]"} />
+      <h2 className={"font-bold text-dark"}>{title}</h2>
       <TechnologiesContainer
-        technologyIcons={data.technologies}
+        technologyIcons={technologies}
         className={"justify-center"}
       />
     </div>
--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,15 +1,17 @@
-import TechnologiesContainer from "../TechnologiesContainer";
 import ProjectItem from "../ProjectItem";
+import data from "../../data-source/projects.json";
 
 export default function Projects() {
   return (
     <section className={"flex w-full p-[5%] justify-between flex-col"}>
       <div className={"flex flex-row-reverse"}>
-        <h1 className={"font-bold text-2xl"}>Projects</h1>
+        <h1 className={"font-bold text-2xl"}>{data.pageTitle}</h1>
       </div>
       <div className={"flex flex-row w-full"}>
-        <div className={"flex flex-col max-w-[40%] gap-4"}>
-          <ProjectItem />
+        <div className={"flex flex-row gap-[100px]"}>
+          {data.projects.map((project) => {
+            return <ProjectItem {...project} />;
+          })}
         </div>
       </div>
     </section>
