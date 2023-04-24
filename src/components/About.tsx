e885a3415fee4344bc4133ffefb77f707790adfc
Alvaro Falcon Morales <alvaro.falconm93@gmail.com>
2023-04-24 20:41:45 +0100

    feat: set data json for about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,15 +1,12 @@
+import data from "../data-source/about.json";
 export default function About() {
   return (
     <section className={"flex w-full p-[5%] justify-between flex-col"}>
       <div className={"flex"}>
-        <h1 className={"font-bold text-2xl"}>About</h1>
+        <h1 className={"font-bold text-2xl"}>{data.title}</h1>
       </div>
       <div className={"flex flex-row-reverse w-full"}>
-        <p className={"max-w-[40%]"}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab at
-          blanditiis, cum debitis dignissimos ex fugit impedit incidunt odit
-          officia quae quia quod sunt, ullam!
-        </p>
+        <p className={"max-w-[40%]"}>{data.text}</p>
       </div>
     </section>
   );
